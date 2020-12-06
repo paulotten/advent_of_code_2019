e9676d3d3fd0e8ee1ae0aedaebb1deafacf1550b
Paul Otten <potten@my.bcit.ca>
2020-12-06 14:25:15 -0500

    Day 10 part 2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,19 +1,61 @@
 mod data;
 
 use std::cmp;
+use std::collections::BTreeMap;
 use std::collections::HashSet;
+use std::f64::consts::PI;
 
-type Point = (usize, usize);
+type Point = (isize, isize);
 
 fn main() {
     let data = data::get_data();
+    //let data = data::_sample();
     let astroids = get_astroids(data);
 
-    puzzle1(astroids);
+    _puzzle2(astroids);
 }
 
-fn puzzle1(astroids: HashSet<Point>) {
+const F2I: f64 = 1_000_000_000.0;
+
+fn _puzzle2(astroids: HashSet<Point>) {
+    let station: Point = (37, 25); // from puzzle 1
+    //let station: Point = (11,13); // sample data
+    let mut a_by_a: BTreeMap<i64, Point> = BTreeMap::new(); // astroids by angle
+
+    // from puzzle 1 we know we can see 309 astroids from this point
+    // we need to find the 200th, so we only need this first scan
+    for a in &astroids {
+        if can_see(station, *a, &astroids) {
+            let rel_x = a.0 - station.0;
+            let rel_y = a.1 - station.1;
+
+            // calculate arc angle
+            let mut angle = f64::atan2(
+                rel_x as f64,
+                -rel_y as f64,
+            );
+
+            if angle < 0.0 {
+                angle += 2.0*PI;
+            }
+
+            // can't hash a float, covert it to an int with 9 of it's decimal places
+            a_by_a.insert((angle * F2I) as i64, *a);
+        }
+    }
+
+    // find the 200th astroid by arc angle
+    let mut a_iter = a_by_a.iter();
+    for i in 1..=200 {
+        let a = a_iter.next().unwrap();
+
+        println!("{}: {},{}", i, a.1.0, a.1.1);
+    }
+}
+
+fn _puzzle1(astroids: HashSet<Point>) {
     let mut max = 0;
+    let mut max_point: Point = (0, 0);
 
     for a in &astroids {
         let mut count = 0;
@@ -26,10 +68,11 @@ fn puzzle1(astroids: HashSet<Point>) {
 
         if count > max {
             max = count;
+            max_point = *a;
         }
     }
 
-    println!("{}", max);
+    println!("{} at {},{}, {}", max, max_point.0, max_point.1, max_point.0*100+max_point.1); // 309 at 37,25
 }
 
 fn can_see(a: Point, b: Point, astroids: &HashSet<Point>) -> bool {
@@ -74,10 +117,7 @@ fn can_see(a: Point, b: Point, astroids: &HashSet<Point>) -> bool {
 
             let y_offest = mult / x_diff;
 
-            if astroids.contains(&(
-                (a.0 as isize + x_offest) as usize,
-                (a.1 as isize + y_offest) as usize,
-            )) {
+            if astroids.contains(&(a.0 + x_offest, a.1 + y_offest)) {
                 return false;
             }
         }
@@ -95,7 +135,7 @@ fn get_astroids(data: &str) -> HashSet<Point> {
 
         for x in 0..chars.len() {
             if chars[x] == '#' {
-                astroids.insert((x, y));
+                astroids.insert((x as isize, y as isize));
             }
         }
 
