dc2744592ab7ee9323125b6bfaf7f64b2158bd40
Paul Otten <potten@my.bcit.ca>
2021-04-26 21:41:15 -0400

    Day 12 part 1

---
+++ day12/src/data.rs
@@ -0,0 +1,13 @@
+pub fn _get_sample1() -> &'static str {
+    "<x=-1, y=0, z=2>
+<x=2, y=-10, z=-7>
+<x=4, y=-8, z=8>
+<x=3, y=5, z=-1>"
+}
+
+pub fn get_data() -> &'static str {
+    "<x=13, y=-13, z=-2>
+<x=16, y=2, z=-15>
+<x=7, y=-18, z=-12>
+<x=-3, y=-8, z=-8>"
+}
---
+++ day12/src/main.rs
@@ -0,0 +1,83 @@
+mod data;
+
+#[derive(Debug)]
+struct Moon {
+    pos: [i32; 3],
+    vel: [i32; 3],
+}
+
+fn main() {
+    // part 1, sample 1
+    //let data = data::_get_sample1();
+    //let steps = 10;
+
+    // part 1
+    let data = data::get_data();
+    let steps = 1000;
+
+    let moons = &mut get_moons(data);
+
+    for _ in 0..steps {
+        simulate_step(moons);
+    }
+
+    println!("{}", get_energy(moons));
+}
+
+fn get_moons(data: &str) -> Vec<Moon> {
+    data.lines().map(|line| {
+        let parts: Vec<_> = line[1..line.len()-1].split(", ").collect();
+        assert!(parts.len() == 3);
+
+        let mut pos = [0; 3];
+
+        for i in 0..pos.len() {
+            let parts: Vec<_> = parts[i].split("=").collect();
+            assert!(parts.len() == 2);
+
+            pos[i] = parts[1].parse().unwrap();
+        }
+
+        Moon {
+            pos,
+            vel: [0, 0, 0],
+        }
+    }).collect()
+}
+
+fn simulate_step(moons: &mut Vec<Moon>) {
+    // update velocities
+    for i in 0..moons.len()-1 {
+        for j in i+1..moons.len() {
+            for axis in 0..3 {
+                if moons[i].pos[axis] < moons[j].pos[axis] {
+                    moons[i].vel[axis] += 1;
+                    moons[j].vel[axis] -= 1;
+                } else if moons[i].pos[axis] > moons[j].pos[axis] {
+                    moons[i].vel[axis] -= 1;
+                    moons[j].vel[axis] += 1;
+                }
+            }
+        }
+    }
+
+    // update positions
+    for i in 0..moons.len() {
+        for axis in 0..3 {
+            moons[i].pos[axis] += moons[i].vel[axis];
+        }
+    }
+}
+
+fn get_energy(moons: &Vec<Moon>) -> i32 {
+    let mut energy = 0;
+
+    for moon in moons {
+        let potential = i32::abs(moon.pos[0]) + i32::abs(moon.pos[1]) + i32::abs(moon.pos[2]);
+        let kinetic = i32::abs(moon.vel[0]) + i32::abs(moon.vel[1]) + i32::abs(moon.vel[2]);
+
+        energy += potential*kinetic;
+    }
+
+    energy
+}
