efe59239fcbda2d0d682617226a3cbba4998a65e
Paul Otten <potten@my.bcit.ca>
2021-04-30 23:07:02 -0400

    Day 14 part 2

--- day14/src/data.rs
+++ day14/src/data.rs
@@ -78,3 +78,16 @@ pub fn _get_sample2() -> &'static str {
 4 C, 1 A => 1 CA
 2 AB, 3 BC, 4 CA => 1 FUEL"
 }
+
+// result 13312
+pub fn _get_sample3() -> &'static str {
+    "157 ORE => 5 NZVS
+165 ORE => 6 DCFZ
+44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
+12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
+179 ORE => 7 PSHF
+177 ORE => 5 HKGWZ
+7 DCFZ, 7 PSHF => 2 XJWVT
+165 ORE => 2 GPVTF
+3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT"
+}
--- day14/src/main.rs
+++ day14/src/main.rs
@@ -11,7 +11,7 @@ struct Reaction {
 #[derive(Debug)]
 struct Component {
     chemical: &'static str,
-    quantity: u32,
+    quantity: u64,
 }
 
 impl Component {
@@ -20,7 +20,7 @@ impl Component {
         assert!(parts.len() == 2);
 
         let chemical = parts[1];
-        let quantity = parts[0].parse::<u32>().unwrap();
+        let quantity = parts[0].parse::<u64>().unwrap();
 
         Component{
             chemical,
@@ -30,7 +30,8 @@ impl Component {
 }
 
 fn main() {
-    part1();
+    //part1();
+    part2();
 }
 
 fn part1() {
@@ -40,14 +41,17 @@ fn part1() {
     // result 165
     //let data = data::_get_sample2();
 
+    // result 13312
+    //let data = data::_get_sample3();
+
     // result 1582325
     let data = data::get_data();
 
     let reactions = get_reactions(data);
     //println!("{:#?}", &reactions);
 
-    let mut requirements: HashMap<&str, u32> = HashMap::new();
-    let mut excess: HashMap<&str, u32> = HashMap::new();
+    let mut requirements: HashMap<&str, u64> = HashMap::new();
+    let mut excess: HashMap<&str, u64> = HashMap::new();
 
     // start by requiring one FUEL
     requirements.insert("FUEL", 1);
@@ -92,7 +96,7 @@ fn part1() {
         // remove the now satisified required chemical
         requirements.remove(target_chemical);
 
-        // record an chemical excess
+        // record any chemical excess
         excess.insert(target_chemical, created_quantity-target_quantity);
 
         println!("{:?}", &requirements);
@@ -100,6 +104,84 @@ fn part1() {
 
     // print our final ORE requirement
     println!("{} ORE", requirements.get("ORE").unwrap());
+}
+
+// wrong, > 631981
+// this is too low because we have left over reagents after making one fuel 
+/*
+fn part2() {
+    println!("{}", 1_000_000_000_000u64 / 1582325u64);
+}
+*/
+
+fn part2() {
+    // result 82892753
+    //let data = data::_get_sample3();
+
+    // result 2267486
+    let data = data::get_data();
+
+    let reactions = get_reactions(data);
+
+    let mut requirements: HashMap<&str, u64> = HashMap::new();
+    let mut excess: HashMap<&str, u64> = HashMap::new();
+
+    requirements.insert("ORE", 0);
+
+    let mut fuel_created: u64 = 0;
+
+    while *requirements.get("ORE").unwrap() < 1_000_000_000_000u64 {
+        requirements.insert("FUEL", 1);
+
+        loop {
+            if requirements.len() == 1 && requirements.contains_key("ORE") {
+                break;
+            }
+
+            // these initial values will never get used due to the `break;` above
+            let mut target_chemical = "";
+            let mut target_quantity = 0;
+
+            // pick a chemical other than ORE
+            for (chemical, quantity) in &requirements {
+                if *chemical == "ORE" {
+                    continue;
+                }
+
+                target_chemical = chemical;
+                target_quantity = *quantity;
+
+                break;
+            }
+
+            // use any excess chemical we have left over
+            let mut created_quantity = *excess.get(target_chemical).unwrap_or(&0);
+
+            // then find the reaction to create more chemical
+            let reaction = reactions.get(target_chemical).unwrap();
+
+            // and create more until we have enough
+            while created_quantity < target_quantity {
+                created_quantity += reaction.output.quantity;
+
+                // and the inputs as further requirements
+                for input in &reaction.inputs {
+                    *requirements.entry(input.chemical).or_insert(0) += input.quantity;
+                }
+            }
+
+            // remove the now satisified required chemical
+            requirements.remove(target_chemical);
+
+            // record any chemical excess
+            excess.insert(target_chemical, created_quantity-target_quantity);
+        }
+
+        fuel_created += 1;
+        println!("{} FUEL, {:012} ORE", fuel_created, *requirements.get("ORE").unwrap());
+    }
+
+    println!("{} FUEL", fuel_created-1);
 }
 
 fn get_reactions(data: &'static str) -> HashMap<&str, Reaction> {
